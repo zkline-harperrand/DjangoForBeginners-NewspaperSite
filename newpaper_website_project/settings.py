7b86974ae3dd938891a40bf2471c12a9febc34e0
Zachary Kline <zkline@harperrand.com>
2026-06-24 15:25:31 -0400

    Chapter11 content - Bootstrap fun

--- newpaper_website_project/settings.py
+++ newpaper_website_project/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "crispy_forms",
+    "crispy_bootstrap5",
     "accounts",
+    "pages"
 ]
 
 MIDDLEWARE = [
@@ -125,3 +128,6 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 AUTH_USER_MODEL = "accounts.CustomUser"
 LOGIN_REDIRECT_URL = "home"
 LOGOUT_REDIRECT_URL = "home"
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
