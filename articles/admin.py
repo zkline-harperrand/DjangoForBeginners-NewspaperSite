66989af1e889c91edf69b75235099a2f0b8d128c
Zachary Kline <zkline@harperrand.com>
2026-07-01 10:36:26 -0400

    Chapter 15 - Comments

--- articles/admin.py
+++ articles/admin.py
@@ -1,11 +1,25 @@
 from django.contrib import admin
-from .models import Article
+from .models import Article, Comment
 
+class CommentInline(admin.TabularInline):
+    model = Comment
+    extra = 0
 class ArticleAdmin(admin.ModelAdmin):
+    inlines = [
+        CommentInline
+    ]
     list_display = [
         "title",
         "body",
         "author",
     ]
+class CommentAdmin(admin.ModelAdmin):
+    list_display = [
+        "author",
+        "comment",
+        "article",
+    ]
+
 
 admin.site.register(Article, ArticleAdmin)
+admin.site.register(Comment, CommentAdmin)
